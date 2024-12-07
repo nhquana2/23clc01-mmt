6f33983dd7bfe8dd36bd0b517c6394f0e1775401
Nguyen Hoang Quan <nhquana2cltt@gmail.com>
2024-12-07 10:34:35 -0500

    add settimeout when client connects server

--- client_modules/connection.py
+++ client_modules/connection.py
@@ -10,5 +10,6 @@ from globals.console import console
 """Connects to a server socket."""
 def connect_server(host, port):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.settimeout(5.0)
     client.connect((host, port))
     return client
